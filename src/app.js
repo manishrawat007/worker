b9e0a80f349032df45a2b09547d04e78c1d41662
Manish Rawat <manish@glimmerworld.com>
2025-01-16 15:17:49 +0530

    crud operations

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const User = require('./models/users')
 const app = express()
 app.use(express.json())
 
+//create user api
 app.post('/signup', async (req, res) => {
     try {
         const user = new User(req.body)
@@ -12,6 +13,53 @@ app.post('/signup', async (req, res) => {
         res.send(newUser)
     } catch (err) {
         res.status(400).send('Some Problem in saving the User')
+    }
+})
+
+// get all user api
+app.get('/users',async(req,res)=>{
+   try{
+   const users= await User.find({})
+   res.send(users)
+   }catch(err){
+    res.status(400).send("users not found--------",err.message)
+   }
+})
+
+// get User by email
+app.post('/userbyemail',async(req,res)=>{
+    const email= req.body.email
+    try{
+    const user= await User.find({email:email})
+    if(user.length==0){
+        res.send("No user found")
+    }
+    res.send(user)
+    }catch(err){
+        res.status(400).send("something went wrong--------",err.message)
+    }
+})
+
+//Update a user by id
+app.patch("/update/:id",async(req,res)=>{
+    const id= req.params.id
+    const data = req.body
+    try{
+        const user = await User.findByIdAndUpdate(id,data)
+        res.send(user)
+    }catch(err){
+        res.status(400).send("something went wrong--------",err.message)
+    }
+})
+
+//Delete a user accout
+app.delete('/user/:id',async(req,res)=>{
+    const id=req.params.id
+    try{
+        await User.findByIdAndDelete(id)
+        res.send("User Details Deleted Successfully")
+    }catch(err){
+        res.status(500).send("something went wrong--------",err.message)
     }
 })
 
