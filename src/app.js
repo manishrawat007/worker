f39134dea972e94e448236da6cf239665f04a492
Manish Rawat <manish@glimmerworld.com>
2025-01-21 17:59:24 +0530

    cors error removed

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors());
 
-app.use(cors({
-  origin: 'http://localhost:3000/',
-  methods: ['GET', 'POST','PUT','PATCH','DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, 
-}));
-
 app.use('/',authRouter)
 app.use('/',userRouter)
 app.use('/',connectionRouter)
