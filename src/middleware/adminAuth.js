8ede576ebf53c44d9d6a13b02e0f1b9f53544586
Manish Rawat <manish@glimmerworld.com>
2025-01-22 18:07:25 +0530

    user feed api with pagination

--- src/middleware/adminAuth.js
+++ src/middleware/adminAuth.js
@@ -5,7 +5,7 @@ const User = require('../models/users')
 const auth = async(req, res, next) => {
     try {
         const token = req.headers['token']
-        console.log('token-------',token)
+        // const {token} = req.cookies
         if (!token) {
             throw new Error("Invalid token");
         }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,41 +1,63 @@
 const express = require('express')
 const userRouter = express.Router()
 const User = require('../models/users')
-const {auth} = require('../middleware/adminAuth')
+const { auth } = require('../middleware/adminAuth')
 const bcrypt = require('bcrypt')
+const ConnectionRequest = require('../models/connections')
 
 // get all user api
 userRouter.get('/users', auth, async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        //remove the self user from feed
+        // remove the users who send request to login user or login user send request to other
+        const limit=req.query.limit || 20
+        const page=req.query.page ||1
+        const skip=(page-1)*limit
+
+        const connection = await ConnectionRequest.find({})
+        let ids = []
+        ids.push(req.user._id)
+        connection.forEach((request) => {
+            if (request.toUserId.equals(req.user._id)) {
+                ids.push(request.fromUserId)
+            } else if (request.fromUserId.equals(req.user._id)) {
+                ids.push(request.toUserId)
+            }
+        })
+
+        const users = await User.find({ _id: { $nin: ids } }).skip(skip).limit(limit)
+        const usersData = users.map(({ firstName, lastName, profile, age, gender, skills, _id, bio }) => {
+            return { _id, firstName, lastName, profile, age, gender, skills, bio }
+        })
+
+        res.json({ data: usersData })
     } catch (err) {
         res.status(400).send("users not found--------" + err.message)
     }
 })
 
 // get user details
-userRouter.get('/profile',auth,async(req,res)=>{
-    const {_id} = req.user
-    try{
-    const user = await User.findById(_id)
-    res.send(user)
+userRouter.get('/profile', auth, async (req, res) => {
+    const { _id } = req.user
+    try {
+        const user = await User.findById(_id)
+        res.send(user)
     }
-    catch(err){
+    catch (err) {
         res.status(404).send('User not found' + err.message)
     }
 
 })
 
 //Update a user by id
-userRouter.patch("/profile/update",auth, async (req, res) => {
-    const {firstName,lastName,password, email, age,gender,profile,bio,skills}=req.body
+userRouter.patch("/profile/update", auth, async (req, res) => {
+    const { firstName, lastName, password, email, age, gender, profile, bio, skills } = req.body
     try {
-        if(firstName|| lastName || password ||email){
+        if (firstName || lastName || password || email) {
             throw new Error("Some of the fields can't be changed");
-            
+
         }
-        const user = await User.findByIdAndUpdate(req.user._id,{age,gender,profile,bio,skills} , {
+        const user = await User.findByIdAndUpdate(req.user._id, { age, gender, profile, bio, skills }, {
             runValidators: true,
             returnDocument: 'after'
         })
@@ -46,21 +68,21 @@ userRouter.patch("/profile/update",auth, async (req, res) => {
 })
 
 // Update the password
-userRouter.post('/profile/changepassword',auth,async(req,res)=>{
-    const {password,newPassword} = req.body
-    try{
-    const {_id} = req.user
-    const user= await User.findOne(_id)
-    const isOldPasswordSame = await bcrypt.compare(password,user.password)
-    if(!isOldPasswordSame){
-      throw new Error("Old password is not same ");
+userRouter.post('/profile/changepassword', auth, async (req, res) => {
+    const { password, newPassword } = req.body
+    try {
+        const { _id } = req.user
+        const user = await User.findOne(_id)
+        const isOldPasswordSame = await bcrypt.compare(password, user.password)
+        if (!isOldPasswordSame) {
+            throw new Error("Old password is not same ");
+        }
+        const passwordHash = await bcrypt.hash(newPassword, 10)
+        const updatePassword = await User.findByIdAndUpdate(_id, { password: passwordHash })
+        res.send(updatePassword)
+    } catch (err) {
+        res.status(400).send(err.message)
     }
-    const passwordHash= await bcrypt.hash(newPassword,10)
-    const updatePassword = await User.findByIdAndUpdate(_id,{password:passwordHash})
-    res.send(updatePassword)
-}catch(err){
-    res.status(400).send(err.message)
-}
 })
 
-module.exports= userRouter
+module.exports = userRouter
